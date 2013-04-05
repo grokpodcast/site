53cf53e3a08a1f0ede22bdc520470ea3c2d51e91
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2013-04-04 23:53:39 -0400

    Added specs for GitHub Pages deployment, FakeFS doesn't work properly

--- _lib/tasks/deploy/github_pages.rb
+++ _lib/tasks/deploy/github_pages.rb
@@ -1,20 +1,25 @@
 # Based on http://ixti.net/software/2013/01/28/using-jekyll-plugins-on-github-pages.html
 
 require 'tmpdir'
+require 'fileutils'
 
 module Tasks
   module Deploy
     module GithubPages
+      extend FileUtils
+
       GITHUB_REPONAME = "grokpodcast/site"
 
       def self.deploy
         Dir.mktmpdir do |tmp|
           cp_r "_site/.", tmp
           Dir.chdir tmp
-          system "git init"
+
           touch ".nojekyll"
-          system "git add ."
+
           message = "Site updated at #{Time.now.utc}"
+          system "git init"
+          system "git add ."
           system "git commit -m #{message.shellescape}"
           system "git remote add origin git@github.com:#{GITHUB_REPONAME}.git"
           system "git push origin master:refs/heads/gh-pages --force"
--- _spec/_lib/tasks/deploy/github_pages_spec.rb
+++ _spec/_lib/tasks/deploy/github_pages_spec.rb
@@ -2,7 +2,57 @@ require_relative '../../../spec_helper'
 require_relative '../../../../_lib/tasks/deploy/github_pages'
 
 describe Tasks::Deploy::GithubPages do
+  let(:task) do
+    result = described_class
+    result.stub(:system) {|arg|   "Executing System: '#{arg}'" }
+    result.stub(:cp_r)   {|*args| "Copying: '#{args}'"         }
+    result.stub(:touch)  {|arg|   "Touching: '#{arg}'"         }
+    result
+  end
+
   context 'deploy' do
-    xit 'copy files to Github Pages'
+    it 'creates a temporary directory' do
+      Dir.should_receive(:mktmpdir)
+      task.deploy
+    end
+
+    context 'inside the temporary directory' do
+      let(:temp_dir) { double('temp_dir') }
+
+      before :each do
+        Dir.should_receive(:mktmpdir).and_yield(temp_dir)
+        Dir.stub(:chdir)
+      end
+
+      it 'copies the generated static site' do
+        task.should_receive(:cp_r).with('_site/.', temp_dir)
+        Dir.should_receive(:chdir).with(temp_dir)
+        task.deploy
+      end
+
+      it 'add .nojekyll file to signal GitHub to not run Jekyll on the site' do
+        task.should_receive(:touch).with('.nojekyll')
+        described_class.deploy
+      end
+
+      it 'setup new git directory and add commit message' do
+        Time.stub_chain(:now, :utc).and_return('now')
+        expected_message = "Site updated at now".shellescape
+
+        task.should_receive(:system).with('git init')
+        task.should_receive(:system).with('git add .')
+        task.should_receive(:system).with("git commit -m #{expected_message}")
+
+        described_class.deploy
+      end
+
+      it 'pushes the new git repo to branch gh-pages on the configured GitHub repo' do
+        task.should_receive(:system).with("git remote add origin git@github.com:#{task::GITHUB_REPONAME}.git")
+        task.should_receive(:system).with('git push origin master:refs/heads/gh-pages --force')
+
+        described_class.deploy
+      end
+    end
+
   end
 end
