c6e000cc2e4f34c15c215def4f8ce0feca398e84
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2013-04-05 00:37:37 -0400

    Moved deployment hard coded params to config file

--- _lib/tasks/deploy/github_pages.rb
+++ _lib/tasks/deploy/github_pages.rb
@@ -1,5 +1,5 @@
 # Based on http://ixti.net/software/2013/01/28/using-jekyll-plugins-on-github-pages.html
-
+require 'jekyll'
 require 'tmpdir'
 require 'fileutils'
 
@@ -8,11 +8,30 @@ module Tasks
     module GithubPages
       extend FileUtils
 
-      GITHUB_REPONAME = "grokpodcast/site"
+      class TargetRepositoryNotConfigured < StandardError
+        def initialize(msg = "Target repository to deploy to GitHub Pages isn't configured")
+          super(msg)
+        end
+      end
+
+      def self.config
+        @config ||= Jekyll.configuration({})
+      end
+
+      def self.payload_directory
+        config['destination']
+      end
+
+      def self.target_repository
+        result = config['deploy']['github_pages']['target_repository']
+        result.nil? ? raise : result
+      rescue
+        raise TargetRepositoryNotConfigured
+      end
 
       def self.deploy
         Dir.mktmpdir do |tmp|
-          cp_r "_site/.", tmp
+          cp_r payload_directory, tmp
           Dir.chdir tmp
 
           touch ".nojekyll"
@@ -21,7 +40,7 @@ module Tasks
           system "git init"
           system "git add ."
           system "git commit -m #{message.shellescape}"
-          system "git remote add origin git@github.com:#{GITHUB_REPONAME}.git"
+          system "git remote add origin git@github.com:#{target_repository}.git"
           system "git push origin master:refs/heads/gh-pages --force"
         end
       end
--- _spec/_lib/tasks/deploy/github_pages_spec.rb
+++ _spec/_lib/tasks/deploy/github_pages_spec.rb
@@ -10,7 +10,50 @@ describe Tasks::Deploy::GithubPages do
     result
   end
 
-  context 'deploy' do
+  before :each do
+    task.instance_variable_set :@config, nil
+  end
+
+  describe 'TargetRepositoryNotConfigured' do
+    it 'has a default error message' do
+      exception = task::TargetRepositoryNotConfigured.new
+      exception.message.should == "Target repository to deploy to GitHub Pages isn't configured"
+    end
+  end
+
+  describe '#config' do
+    it "loads Jekyll's config" do
+      Jekyll.stub(:configuration).with({}).and_return(:config)
+      task.config.should == :config
+    end
+
+    it "memoizes the result" do
+      Jekyll.should_receive(:configuration).with({}).once.and_return(:config)
+      task.config.should == :config
+      task.config.should == :config
+    end
+  end
+
+  describe 'payload_directory' do
+    it 'gets the payload directory from config' do
+      task.stub(:config).and_return({'destination' => :payload})
+      task.payload_directory.should == :payload
+    end
+  end
+
+  describe 'target_repository' do
+    it 'gets the target repository from config' do
+      task.stub(:config).and_return({'deploy' => {'github_pages' => {'target_repository' => :target}}})
+      task.target_repository.should == :target
+    end
+
+    it "raises an error if the repo isn't configured" do
+      task.stub(:config).and_return({})
+      expect { task.target_repository }.to raise_error(task::TargetRepositoryNotConfigured)
+    end
+  end
+
+  describe '#deploy' do
     it 'creates a temporary directory' do
       Dir.should_receive(:mktmpdir)
       task.deploy
@@ -20,19 +63,24 @@ describe Tasks::Deploy::GithubPages do
       let(:temp_dir) { double('temp_dir') }
 
       before :each do
-        Dir.should_receive(:mktmpdir).and_yield(temp_dir)
+        Dir.stub(:mktmpdir).and_yield(temp_dir)
         Dir.stub(:chdir)
+        task.stub(:payload_directory).and_return('stub dir')
+        task.stub(:target_repository).and_return('stub repo')
       end
 
       it 'copies the generated static site' do
-        task.should_receive(:cp_r).with('_site/.', temp_dir)
+        task.stub(:payload_directory).and_return('payload')
+
+        task.should_receive(:cp_r).with('payload', temp_dir)
         Dir.should_receive(:chdir).with(temp_dir)
+
         task.deploy
       end
 
       it 'add .nojekyll file to signal GitHub to not run Jekyll on the site' do
         task.should_receive(:touch).with('.nojekyll')
-        described_class.deploy
+        task.deploy
       end
 
       it 'setup new git directory and add commit message' do
@@ -43,16 +91,17 @@ describe Tasks::Deploy::GithubPages do
         task.should_receive(:system).with('git add .')
         task.should_receive(:system).with("git commit -m #{expected_message}")
 
-        described_class.deploy
+        task.deploy
       end
 
       it 'pushes the new git repo to branch gh-pages on the configured GitHub repo' do
-        task.should_receive(:system).with("git remote add origin git@github.com:#{task::GITHUB_REPONAME}.git")
+        task.stub(:target_repository).and_return('user/repo')
+
+        task.should_receive(:system).with("git remote add origin git@github.com:user/repo.git")
         task.should_receive(:system).with('git push origin master:refs/heads/gh-pages --force')
 
-        described_class.deploy
+        task.deploy
       end
     end
-
   end
 end
