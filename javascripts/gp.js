2e6056712e0e402cf29ffe21f9b88663ecb23d7a
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2014-10-21 14:40:33 -0200

    Clean up js and added mobile menu script

--- javascripts/gp.js
+++ javascripts/gp.js
@@ -16,41 +16,23 @@ if(typeof(console.log) != 'function') console.log = function(){};
 var App = {
 
     StartApp: function() {
-        try {
-            this.InterfaceActions();
-            this.FormActions();
-        } catch (e) {
-            alert('Existem erros no script.');
-            console.log('Error: ' + e);
-        }
+      try {
+        this.InterfaceActions();
+      } catch (e) {
+        alert('Existem erros no script.');
+        console.log('Error: ' + e);
+      }
     },
 
     InterfaceActions: function() {
-		
-		
 
-    }, // InterfaceActions
+      /* Menu: Mobile */
 
-    FormActions: function() {
+      $("#nav-trigger").click(function() {
+        $("#topbar ul").toggle();
+      });
 
-		// Focus
-		$('input[type="text"], input[type="password"], textarea').focus(function() {
-			$(this).css("box-shadow", "#ccc 0 0 15px");
-		});
-		$('input[type="text"], input[type="password"], textarea').blur(function() {
-			$(this).css("box-shadow", "none");
-		});
-		$('input[type="text"], input[type="password"], textarea').focus(function() {
-			var li = $(this).parent("li")
-			li.data('background', li.css('background-color'));
-			li.css("background-color", "#e1e1e1");
-		});
-		$('input[type="text"], input[type="password"], textarea').blur(function() {
-			var li = $(this).parent("li");
-			li.css("background-color", li.data('background'));
-		});
-
-    } // FormActions
+    }
 
 } // Var Site
 
